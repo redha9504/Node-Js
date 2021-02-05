f41059d2c815354ed213a5a5ad27822d337eb7e9
Redha DETTI <redha.d95@hotmail.fr>
2021-02-05 11:05:21 +0100

    mon td 1

---
+++ soap-server/index.js
@@ -0,0 +1,72 @@
+var myService = {
+      MyService: {
+          MyPort: {
+              HelloWorld: function(args) {
+                  return {
+                      name: args.name
+                  };
+              },
+
+              // This is how to define an asynchronous function with a callback.
+              MyAsyncFunction: function(args, callback) {
+                  // do some work
+                  callback({
+                      name: args.name
+                  });
+              },
+
+              // This is how to define an asynchronous function with a Promise.
+              MyPromiseFunction: function(args) {
+                  return new Promise((resolve) => {
+                    // do some work
+                    resolve({
+                      name: args.name
+                    });
+                  });
+              },
+
+              // This is how to receive incoming headers
+              HeadersAwareFunction: function(args, cb, headers) {
+                  return {
+                      name: headers.Token
+                  };
+              },
+
+              // You can also inspect the original `req`
+              reallyDetailedFunction: function(args, cb, headers, req) {
+                  console.log('SOAP `reallyDetailedFunction` request from ' + req.connection.remoteAddress);
+                  return {
+                      name: headers.Token
+                  };
+              }
+          }
+      }
+  };
+
+  var xml = require('fs').readFileSync('myservice.wsdl', 'utf8');
+
+  //http server example
+  var server = require('http').createServer(function(req, res){
+      res.writeHead(200);
+      res.write('Hello World');
+      res.end();
+  });
+
+  var soap = require('soap')
+
+  server.listen(8000);
+  soap.listen(server, '/wsdl', myService, xml, function(){
+    console.log('server initialized');
+  });
+
+  //express server example
+  /*var app = express();
+  //body parser middleware are supported (optional)
+  app.use(bodyParser.raw({type: function(){return true;}, limit: '5mb'}));
+  app.listen(8001, function(){
+      //Note: /wsdl route will be handled by soap module
+      //and all other routes & middleware will continue to work
+      soap.listen(app, '/wsdl', myService, xml, function(){
+        console.log('server initialized');
+      });
+  });*/
